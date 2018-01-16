f0bdd9e381739851bf977a554d0fe20ba727e465
Marta P <mapa.startuj@gmail.com>
2018-01-16 20:53:52 +0100

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -25,13 +25,20 @@ if (env === 'production') {
 module.exports = {
     entry: (env !== 'production' ? [
         'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
+        //'webpack-dev-server/client?http://localhost:8080',
+        //'webpack/hot/only-dev-server',
     ] : []).concat(['./client/index.js']),
     
     output: {
         filename: './bundle.js',
         path: path.resolve(__dirname, 'public'),
+    },
+
+    devServer: {
+        proxy: { 
+          '/': 'http://localhost:3000'
+        },
+        open: true
     },
 
     module: {
